818de6e5a72941d50125f7c9e8af9f252fdc1c7d
Garion's Blade <yuxuanng@hotmail.com>
2019-01-13 15:36:06 -0600

    added input

--- bagdrop/src/components/resultsScreen/index.js
+++ bagdrop/src/components/resultsScreen/index.js
@@ -5,10 +5,31 @@ import data from '../../data/data.json'
 import './style.scss'
 
 class ResultsScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchParams: {
+                to: '',
+                from: '',
+                date: '',
+            }
+        };
+    }
+
+    onFromChange = (from) => {this.setState({searchParams: {
+            from: from
+        }})};
+    onToChange = (from) => {this.setState({searchParams: {
+            from: from
+        }})};
+    onDateChange = (from) => {this.setState({searchParams: {
+            from: from
+        }})};
+
     render() {
         return (
             <div className='results-screen'>
-                <SearchInfo searchParams={this.props.searchParams}/>
+                <SearchInfo searchParams={this.state.searchParams} onFromChange={this.onFromChange} onToChange={this.onToChange} onDateChange={this.onDateChange}/>
                 <ResultsShelf listings={data.listings} />
             </div>
         )
--- bagdrop/src/components/searchInfo/index.js
+++ bagdrop/src/components/searchInfo/index.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 class SearchInfo extends Component {
+
+    handleFromChange(e) {
+        this.props.onFromChange(e.target.value)
+    }
+    handleToChange(e) {
+        this.props.onToChange(e.target.value)
+    }
+    handleDateChange(e) {
+        this.props.onDateChange(e.target.value)
+    }
     render(){
         const searchParams = this.props.searchParams;
         return (
             <div>
-                <div>{searchParams.from}</div>
-                <div>{searchParams.to}</div>
-                <div>{searchParams.date}</div>
+                <input type="text" name="From" value={searchParams.from} onChange={this.handleFromChange}/>
+                <input type="text" name="To" value={searchParams.to} onChange={this.handleToChange}/>
+                <input type="text" name="Date" value={searchParams.date} onChange={this.handleDateChange}/>
             </div>
 
         )
