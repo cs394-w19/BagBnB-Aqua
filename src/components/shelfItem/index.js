3f0df165324c79e09629543c920d1a7fea2221e9
Garion's Blade <yuxuanng@hotmail.com>
2019-01-24 19:48:01 -0600

    trying automatic deployment

--- src/components/shelfItem/index.js
+++ src/components/shelfItem/index.js
@@ -1,71 +1,45 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./style.scss";
 
-const classNames = require("classnames");
 
 class ShelfItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isBold: false
-    };
-  }
 
-  onClick = () => {
-    const prevState = this.state.isBold;
-    this.setState({ isBold: !prevState });
-  };
+    render() {
+        const listing = this.props.listing;
+        const flightInfo = listing.flightInfo;
+        return (
+            <div className="shelf-item">
+                <div className="shelf-item-info">
+                    <div className="shelf-item-info-flight">
+                        <div className="shelf-item-info-flight-vendor">{listing.vendorName}</div>
+                        <div className="shelf-item-info-flight-number">{flightInfo.flightNumber}</div>
+                        <div className="shelf-item-info-flight-location">
+                            <span>{flightInfo.departureLoc}</span> &#8594;
+                            <span> {flightInfo.arrivalLoc}</span>
+                        </div>
+                        <div className="shelf-item-info-flight-time">
+                            <span>{flightInfo.departureTime}</span>
+                            &nbsp;&mdash;&nbsp;
+                            <span>{flightInfo.arrivalTime}</span>
+                        </div>
+                    </div>
 
-  // formatDate = l => {
-  //   let date = toString(l.date);
-  //   let dep = toString(l.departureTime);
-  //   let arr = toString(l.arrivalTime);
-  //   let year = date.split("-")[0];
-  //   let month = date.split("-")[1];
-  //   let day = date.split("-")[2];
-  //   let formattedDate =
-  //     toString(month) + "/" + toString(day) + "/" + toString(year);
-  //   return formattedDate;
-  // };
+                    <div className="shelf-item-info-baggage">
+                        <span> ${listing.price}</span>
+                        <span>{listing.weight}lb</span>
 
-  render() {
-    const listing = this.props.listing;
-    const flightInfo = listing.flightInfo;
-    const classname = classNames("shelf-item", {
-      "shelf-item-bold": this.state.isBold
-    });
-    return (
-      <div className={classname} onClick={this.onClick}>
-        <div className="shelf-item-info">
-          <div className="shelf-item-info-flight">
-            <div className="shelf-item-info-flight-vendor">
-              <span>{listing.vendorName}</span>
+                        <button
+                            className={"shelf-item-book-button"}
+                            onClick={() => this.props.onBookClick(listing.id, "matthewa")}
+                        >
+                            Book
+                        </button>
+                    </div>
+                </div>
+
             </div>
-            <div className="shelf-item-info-flight-location">
-              <span>{flightInfo.departureLoc}</span> &#8594;
-              <span> {flightInfo.arrivalLoc}</span>
-            </div>
-            <div className="shelf-item-info-flight-time">
-              <span>{flightInfo.departureTime}</span>
-              &nbsp;&mdash;&nbsp;
-              <span>{flightInfo.arrivalTime}</span>
-            </div>
-          </div>
-
-          <div className="shelf-item-info-baggage">
-            <div> ${listing.price}</div>
-            <div>{listing.weight}lb</div>
-          </div>
-        </div>
-        <button
-          className={"shelf-item-book-button"}
-          onClick={() => this.props.onBookClick(listing.id, "matthewa")}
-        >
-          Book
-        </button>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default ShelfItem;
