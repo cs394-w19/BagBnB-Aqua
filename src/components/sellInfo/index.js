661c492c1d3d3d6fb5b01c9ed96e683b3ae590e0
Harish Mundluru <19980524+HarishMundluru1998@users.noreply.github.com>
2019-02-02 17:12:04 -0600

    Big cuts in code
    Cuts in code to improve seller side

--- src/components/sellInfo/index.js
+++ src/components/sellInfo/index.js
@@ -4,52 +4,21 @@ import "./style.scss";
 //const classname = require('classnames');
 
 class SellInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.handleFromChange = this.handleFromChange.bind(this);
-        this.handleToChange = this.handleToChange.bind(this);
-        this.handleDateChange = this.handleDateChange.bind(this);
-        this.handleFlightNumberChange = this.handleFlightNumberChange.bind(this);
-    }
-
-    handleFromChange(e) {
-        this.props.onFromChange(e.target.value)
-    }
-
-    handleToChange(e) {
-        this.props.onToChange(e.target.value)
-    }
-
-    handleDateChange(e) {
-        this.props.onDateChange(e.target.value)
-    }
-
-    handleFlightNumberChange(e) {
-        this.props.onFlightNumberChange(e.target.value)
-    }
 
     render() {
-        const {searchParams}= this.props;
-//        const flightNumberClassName = classname(
-//            "search-flight-tab-button",
-//            {"search-flight-tab-button-active": searchUsingFlightNumber})
-//        const locationClassName = classname(
-//            "search-flight-tab-button",
-//            {"search-flight-tab-button-active": !searchUsingFlightNumber})
+        let price = 30, weight = 50, flightNumber, date;
+        let username = "karenk";
         return (
-            <div className="search">
-                <form className="search-input">
-                    <input placeholder="Departure (i.e., ORD)" 
-                        type="text" value={searchParams.from} onChange={this.handleFromChange}/>
-                    <input placeholder="Arrival (i.e., LGA)"
-                        type="text" value={searchParams.to} onChange={this.handleToChange}/>
-                    <input placeholder="Flight Number (i.e VA314)"
-                        type="text"
-                        value={searchParams.flightNumber}
-                        onChange={this.handleFlightNumberChange}/>
+            <div className="listing-form">
+                <form className="listing-form-input">
+                    <input placeholder="price" 
+                        type="text" value={price}/>
+                    <input placeholder="weight"
+                        type="text" value={weight}/>
+                    <input placeholder=""
+                        type="text" value={flightNumber}/>
                     <input
-                        type="date"
-                        value={searchParams.date} onChange={this.handleDateChange}/>
+                        type="date" value={date}/>
                 </form>
             </div>
         )
--- src/components/sellLuggageAllowance/index.js
+++ src/components/sellLuggageAllowance/index.js
@@ -5,73 +5,14 @@ import './style.scss'
 import SellInfo from '../sellInfo'
 
 class SellLuggageAllowance extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchParams: {
-                to: '',
-                from: '',
-                date: new Date().toISOString(),
-                flightNumber: ''
-            },
-            searchUsingFlightNumber: true
-        };
-        this.onBookClick = this.onBookClick.bind(this);
-    }
-
-    onFlightClick = () => {
-        const state = this.state;
-        state.searchUsingFlightNumber = true;
-        this.setState(state);
-    };
-
-    onLocationClick = () => {
-        const state = this.state;
-        state.searchUsingFlightNumber = false;
-        this.setState(state);
-    };
-
-    onFromChange = (from) => {
-        const state = this.state;
-        state.searchParams.from = from;
-        this.setState(state);
-    };
-    onToChange = (to) => {
-        const state = this.state;
-        state.searchParams.to = to;
-        this.setState(state);
-    };
-    onDateChange = (date) => {
-        const state = this.state;
-        state.searchParams.date = date;
-        this.setState(state);
-    };
-    onFlightNumberChange = (flightNumber) => {
-        const state = this.state;
-        state.searchParams.flightNumber = flightNumber;
-        this.setState(state);
-    };
-
-    onBookClick = (listingId) => {
-        this.props.history.push({
-            pathname: "/confirmation",
-            search: "?id=" + listingId
-        });
-    }
+    
 
     render() {
         return (
             <div className='results-screen'>
-            <div className="header-styling"><h1>Enter Luggage Allowance Details:</h1></div>
+            <div className="header-styling"><h1>Enter Flight and Baggage Details:</h1></div>
                 <SellInfo
-                    searchParams={this.state.searchParams}
-                    onFromChange={this.onFromChange}
-                    onToChange={this.onToChange}
-                    onDateChange={this.onDateChange}
-                    onFlightNumberChange={this.onFlightNumberChange}
-                    onFlightClick={this.onFlightClick}
-                    onLocationClick={this.onLocationClick}
-                    searchUsingFlightNumber={this.state.searchUsingFlightNumber}
+
                 />
             </div>
         )
